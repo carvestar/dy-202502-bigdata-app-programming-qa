688f9b253df2f77d69c00caae27294401e5a5500
214 <214@gmail.com>
2025-09-19 15:26:28 +0900

    3주차 실습파일

---
+++ ch03/sample28.py
@@ -0,0 +1,10 @@
+'''
+다음 소스 코드는 40명의 학생에게 50~100점 사이의 점수를 무작위로 할당한 딕셔너리 scores를 제공한다.
+각 문제의 정답을 제시하기 위한 소스 코드를 작성하고 결과를 입력하시오.
+'''
+
+import random
+
+scores = dict()
+for i in range(10, 50):
+    scores['S' + str(i)] = random.randrange(50, 100)
